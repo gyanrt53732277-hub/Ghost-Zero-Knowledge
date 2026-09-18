12b7e23db16f9de2945d0fcf5bd2f375a463ecdb
Akshat <gyanrt53732277@gmail.com>
2026-09-18 18:45:08 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,3 +1,12 @@
+from pathlib import Path
 import zipfile
-with zipfile.ZipFile('/home/gyanrt53732277-hub/.compact/versions/0.31.1/x86_64-unknown-linux-musl/artifact.zip', 'r') as z:
-    z.extractall('/home/gyanrt53732277-hub/.compact/versions/0.31.1/x86_64-unknown-linux-musl/')
+
+artifact = Path(
+    "/home/gyanrt53732277-hub/.compact/versions/0.31.1/"
+    "x86_64-unknown-linux-musl/artifact.zip"
+)
+
+destination = artifact.parent
+
+with zipfile.ZipFile(artifact) as archive:
+    archive.extractall(destination)

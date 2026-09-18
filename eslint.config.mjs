f328feb1f9b2ef9116226527e8504dc8107763dd
Akshat <gyanrt53732277@gmail.com>
2026-09-18 18:46:44 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,49 +1,59 @@
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 import reactPlugin from "eslint-plugin-react";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import globals from "globals";
+import prettierConfig from "eslint-plugin-prettier/recommended";
 
 const config = tseslint.config(
   {
-    ignores: ["eslint.config.mjs", "src/managed/"],
+    ignores: [
+      "eslint.config.mjs",
+      "src/managed/",
+    ],
   },
+
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   reactPlugin.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
+  prettierConfig,
+
   {
-    ignores: ["src/managed"],
-    rules: {
-      "@typescript-eslint/no-misused-promises": "off", // https://github.com/typescript-eslint/typescript-eslint/issues/5807
-      "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/promise-function-async": "off",
-      "@typescript-eslint/no-redeclare": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-unsafe-call": "warn",
-      "@typescript-eslint/no-unsafe-return": "warn",
-      "@typescript-eslint/no-unsafe-assignment": "warn",
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/no-unsafe-argument": "warn",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
+    ignores: ["src/managed/"],
+
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.jest,
       },
+
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/promise-function-async": "off",
+      "@typescript-eslint/no-redeclare": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
     },
   },
 );
